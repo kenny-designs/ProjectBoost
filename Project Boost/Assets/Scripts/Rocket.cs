cc401514f43354409870a1b62277d31f34e92d1c
Alexander Aguilar <alexanderman2004@yahoo.com>
2019-04-15 19:54:27 -0400

    Finished lesson 54. Prefabs In Detail

--- Project Boost/Assets/Scripts/Rocket.cs
+++ Project Boost/Assets/Scripts/Rocket.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour {
+  // Config
+  [SerializeField] float rcsThrust = 100f;
+  [SerializeField] float mainThrust = 100f;
+
   // Cached component references
   Rigidbody myRigidbody;
   AudioSource audioSource;
@@ -19,9 +23,18 @@ public class Rocket : MonoBehaviour {
     Rotate();
   }
 
+  private void OnCollisionEnter(Collision collision) {
+    switch (collision.gameObject.tag) {
+      case "Friendly":
+        break;
+      default:
+        break;
+    }
+  }
+
   private void Thrust() {
     if (Input.GetKey(KeyCode.Space)) {
-      myRigidbody.AddRelativeForce(Vector3.up);
+      myRigidbody.AddRelativeForce(Vector3.up * mainThrust);
       if (!audioSource.isPlaying) {
         audioSource.Play();
       }
@@ -34,11 +47,13 @@ public class Rocket : MonoBehaviour {
   private void Rotate() {
     myRigidbody.freezeRotation = true;
 
+    float rotationThisFrame = rcsThrust * Time.deltaTime;
+
     if (Input.GetKey(KeyCode.A)) {
-      transform.Rotate(Vector3.forward);
+      transform.Rotate(Vector3.forward * rotationThisFrame);
     }
     else if (Input.GetKey(KeyCode.D)) {
-      transform.Rotate(-Vector3.forward);
+      transform.Rotate(-Vector3.forward * rotationThisFrame);
     }
 
     myRigidbody.freezeRotation = false;
