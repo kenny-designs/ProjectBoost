47e78883366642e55a2916b473d1d2477a9c1498
Alexander Aguilar <alexanderman2004@yahoo.com>
2019-04-14 23:23:45 -0400

    Finished lesson 47. Adding A Touch Of Audio

---
+++ Project Boost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+  // Cached component references
+  Rigidbody myRigidbody;
+  AudioSource audioSource;
+
+  private void Start() {
+    myRigidbody = GetComponent<Rigidbody>();
+    audioSource = GetComponent<AudioSource>();
+  }
+
+  // Update is called once per frame
+  void Update() {
+    ProcessInput();
+  }
+
+  private void ProcessInput() {
+    if (Input.GetKey(KeyCode.Space)) {
+      myRigidbody.AddRelativeForce(Vector3.up);
+      if (!audioSource.isPlaying) {
+        audioSource.Play();
+      }
+    }
+    else {
+      audioSource.Stop();
+    }
+
+    if (Input.GetKey(KeyCode.A)) {
+      transform.Rotate(Vector3.forward);
+    }
+    else if (Input.GetKey(KeyCode.D)) {
+      transform.Rotate(-Vector3.forward);
+    }  
+  }
+}
