70aa8a0730d6c7ae6f745ec65d888976476a4a62
Alexander Aguilar <alexanderman2004@yahoo.com>
2019-04-16 20:21:13 -0400

    Finished lesson 66. Make Game Moments

---
+++ Project Boost/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// only allows one script of this type on a GameObject
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour {
+  // Config
+  [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+  [SerializeField] float period = 2f;
+
+  float movementFactor;
+  Vector3 startingPos;
+
+  // Start is called before the first frame update
+  void Start() {
+    startingPos = transform.position;
+  }
+
+  // Update is called once per frame
+  void Update() {
+    if (period <= Mathf.Epsilon) { return; }
+    float cycles = Time.time / period;
+
+    const float tau = 2 * Mathf.PI;
+    float rawSinWave = Mathf.Sin(cycles * tau);
+
+    movementFactor = rawSinWave / 2f + 0.5f;
+    Vector3 offset = movementFactor * movementVector;
+    transform.position = startingPos + offset;
+  }
+}
--- Project Boost/Assets/Scripts/Rocket.cs
+++ Project Boost/Assets/Scripts/Rocket.cs
@@ -2,15 +2,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
   // Config
   [SerializeField] float rcsThrust = 100f;
   [SerializeField] float mainThrust = 100f;
+  [SerializeField] float levelLoadDelay = 2f;
+
+  [SerializeField] AudioClip mainEngine;
+  [SerializeField] AudioClip deathSFX;
+  [SerializeField] AudioClip successSFX;
+
+  [SerializeField] ParticleSystem mainEngineParticles;
+  [SerializeField] ParticleSystem deathParticles;
+  [SerializeField] ParticleSystem successParticles;
 
   // Cached component references
   Rigidbody myRigidbody;
   AudioSource audioSource;
+
+  enum State { Alive, Dying, Transcending }
+  State state = State.Alive;
 
   private void Start() {
     myRigidbody = GetComponent<Rigidbody>();
@@ -19,32 +32,71 @@ public class Rocket : MonoBehaviour {
 
   // Update is called once per frame
   void Update() {
-    Thrust();
-    Rotate();
+    if (state == State.Alive) {
+      RespondToThrustInput();
+      RespondToRotateInput();
+    }
   }
 
   private void OnCollisionEnter(Collision collision) {
+    // ignore collisions when dead
+    if (state != State.Alive) { return; }
+
     switch (collision.gameObject.tag) {
       case "Friendly":
         break;
+      case "Finish":
+        StartSuccessSequence();
+        break;
       default:
+        StartDeathSequence();
         break;
     }
   }
 
-  private void Thrust() {
+  private void StartSuccessSequence() {
+    state = State.Transcending;
+    audioSource.Stop();
+    audioSource.PlayOneShot(successSFX);
+    successParticles.Play();
+    Invoke("LoadNextLevel", levelLoadDelay);
+  }
+
+  private void StartDeathSequence() {
+    state = State.Dying;
+    audioSource.Stop();
+    audioSource.PlayOneShot(deathSFX);
+    deathParticles.Play();
+    Invoke("LoadFirstLevel", levelLoadDelay);
+  }
+
+  private void LoadNextLevel() {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+  }
+
+  private void LoadFirstLevel() {
+    SceneManager.LoadScene(0);
+  }
+
+  private void RespondToThrustInput() {
     if (Input.GetKey(KeyCode.Space)) {
-      myRigidbody.AddRelativeForce(Vector3.up * mainThrust);
-      if (!audioSource.isPlaying) {
-        audioSource.Play();
-      }
+      ApplyThrust();
     }
     else {
       audioSource.Stop();
+      mainEngineParticles.Stop();
     }
   }
 
-  private void Rotate() {
+  private void ApplyThrust() {
+    myRigidbody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+    if (!audioSource.isPlaying) {
+      audioSource.PlayOneShot(mainEngine);
+    }
+    mainEngineParticles.Play();
+  }
+
+  private void RespondToRotateInput() {
     myRigidbody.freezeRotation = true;
 
     float rotationThisFrame = rcsThrust * Time.deltaTime;
